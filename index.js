1e92bb300033f02e2457a2a1af62949bd6395ea4
yukiya4242 <126574597+yukiya4242@users.noreply.github.com>
2023-06-18 08:12:43 +0000

    Test Refactor

---
+++ index.js
@@ -0,0 +1,29 @@
+module.exports = Phrase;
+
+// reverse()メソッドを追加してすべてのStringで使えるようにする
+String.prototype.reverse = function() {
+  return Array.from(this).reverse().join("");
+}
+
+// Phraseオブジェクトを定義する
+function Phrase(content) {
+  this.content = content;
+
+  // パリンドロームのテスト用に変換した訳文を返す
+  this.processedContent = function processedContent() {
+    return this.letters().toLowerCase();
+  }
+
+ // コンテンツの文字だけを返す
+  // 利用例:
+  //   new Phrase("Hello, world!").letters() === "Helloworld"
+  this.letters = function letters() {
+    return (this.content.match(/[a-z]/gi) || []).join("");
+
+  }
+
+  // パリンドロームならtrueを、違うならfalseを返す
+  this.palindrome = function palindrome() {
+    return this.processedContent() === this.processedContent().reverse();
+  }
+}
--- test/test.js
+++ test/test.js
@@ -14,5 +14,22 @@ describe("Phrase", function() {
       let plainPalindrome = new Phrase("racecar");
       assert(plainPalindrome.palindrome());
     });
+
+    it("パリンドロームが大文字小文字混じりでもtrueを返す", function() {
+      let mixedCase = new Phrase("RaceCar");
+      assert(mixedCase.palindrome());
+    });
+
+    it("パリンドロームに句読点があってもtrueを返す", function() {
+      let punctuatedPalindrome = new Phrase("Madam, I'm Adam.");
+      assert(punctuatedPalindrome.palindrome());
+    });
+
+    describe("#letters", function() {
+    it("文字だけを返す", function() {
+      let punctuatedPalindrome = new Phrase("Madam, I'm Adam.");
+      assert.strictEqual(punctuatedPalindrome.letters(), "MadamImAdam");
+    });
+    });
   });
 });
